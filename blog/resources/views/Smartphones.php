b837fb8aa44e98a5ef56fd2dffab3bc8ae56143d
zekev <zekeev96@gmail.com>
2019-10-03 20:10:21 -0500

    Quiubole que cual

---
+++ blog/resources/views/Smartphones.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+		<html lang="en">
+		<head>
+			<meta charset="UTF-8">
+
+<!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+ 
+
+			<title>Document</title>
+
+		</head>
+
+		<body>
+
+
+			<h1>Smartphones</h1>
+
+	<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="/Imagenes/dogosovietico.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="/Imagenes/dogosovietico.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="dogosovietico.jpg" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+	
+			<div class="card-deck">
+  <div class="card">
+    <img src="/Imagenes/lel.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+
+  <div class="card">
+    <img src="C:/Imagenes/lel.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Motorola G6 play</h5>
+      <p class="card-text">Nuevo telefono inteligente</p>
+      
+    </div>
+  </div>
+
+
+  <div class="card">
+    <img src="C:/Imagenes/lel.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+</div>
+
+		</body>
+
+
+		</html>		
+	
---
+++ blog/routes/web.php
@@ -0,0 +1,46 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('inicio', function () {
+    return view('inicio');
+});
+
+
+Route::get('Computacion', function () {
+    return view('Computacion');
+});
+
+Route::get('Smartphones', function () {
+    return view('Smartphones');
+});
+
+Route::get('Zapatos', function () {
+    return view('Zapatos');
+});
+
+Route::get('Ropa', function () {
+    return view('Ropa');
+});
+
+Route::get('/Electronicos', function () {
+    return view('Electronicos');
+});
+
+Route::get('/JorgeAnotinioliraMartinez', function () {
+    return view('JorgeAnotinioliraMartinez');
+});
+
