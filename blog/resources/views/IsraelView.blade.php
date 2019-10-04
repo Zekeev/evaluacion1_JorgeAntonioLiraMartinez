86dd87f5fb51bd5d96ce6409f82966688b29a4ce
Israel Torres <israeltorres@uadec.edu.mx>
2019-10-03 20:36:49 -0500

    Cambio Israel Torres

---
+++ blog/resources/views/IsraelView.blade.php
@@ -0,0 +1,1 @@
+echo "Hola jorge jaja";
--- blog/routes/web.php
+++ blog/routes/web.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('IsraelView', function () {
+    return view('IsraelView');
+});
+
 Route::get('inicio', function () {
     return view('inicio');
 });
